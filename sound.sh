cbeb1dbb51d5759084f96a7c3d039fde141a30ad
Palash <palash.kanti.kundu.90@gmail.com>
2023-01-07 20:11:56 +0530

    Add Robo voice reminders

--- sound.sh
+++ sound.sh
@@ -2,8 +2,20 @@
 #!/bin/bash
 # Must Install Sox
 
+counter=0
+
 while :
 do
+    if [ `expr $counter % 4` -eq 0 ]
+    then
+        echo "Take a break" | text2wave -o a.wav && play a.wav
+    fi
+
+    if [ `expr $counter % 5` -eq 0 ]
+    then
+        echo "Break Over" | text2wave -o a.wav && play a.wav
+    fi
+
     number=$(pacmd list-sink-inputs | grep -c "state: RUNNING")
     echo "$number"
     if [ $number -lt 1 ]
@@ -11,5 +23,6 @@ do
         echo "Play Synth"
         play -n synth 0.0005 sin 20 
     fi	
-    sleep 600
+    counter=`expr $counter + 1`
+    sleep 300
 done
