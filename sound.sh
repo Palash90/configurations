7b1f173fc1238b328c891defe279b797e5aa63fe
Palash <palash.kanti.kundu.90@gmail.com>
2023-01-07 19:12:25 +0530

    Change sleep

--- sound.sh
+++ sound.sh
@@ -4,12 +4,12 @@
 
 while :
 do
-	number=$(pacmd list-sink-inputs | grep -c "state: RUNNING")
-	echo "$number"
-	if [ $number -lt 1 ]
-	then
-		echo "Play Synth"
-		play -n synth 0.0005 sin 20 
-	fi	
-	sleep 1
+    number=$(pacmd list-sink-inputs | grep -c "state: RUNNING")
+    echo "$number"
+    if [ $number -lt 1 ]
+    then
+        echo "Play Synth"
+        play -n synth 0.0005 sin 20 
+    fi	
+    sleep 600
 done
